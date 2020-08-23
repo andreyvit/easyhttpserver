86bc959820e82af9cf0d4f5b36e7ed9ccc037095
Andrey Tarantsov <andrey@tarantsov.com>
2020-08-23 17:34:54 +0300

    Add package doc

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+easyhttpserver runs production-ready, Let's Encrypt-enabled HTTP server
+in a few lines of code.
+
+Features HTTP, optional HTTPS with Let's Encrypt, graceful shutdown,
+Heroku-style 12-factor configuration, and development mode.
+*/
+package easyhttpserver
